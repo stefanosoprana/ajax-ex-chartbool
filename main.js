58f6ed0de6486f42fe4e1312e7d6660008cd35c4
stefanosoprana <stefano.soprana@icloud.com>
2019-01-09 23:53:51 +0100

    Secondo grafico

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ $(document).ready(function(){
         arrayLabels.push(key);
         arrayData.push(oggetto[key])
       }
-      var ctx = $('.dashboard')
+      var ctx = $('.dashboard1')
       var myLineChart = new Chart(ctx, {
         type: 'line',
         data: {
@@ -30,11 +30,45 @@ $(document).ready(function(){
           datasets: [{
             label: "Vendite",
             borderColor: 'rgb(255, 99, 132)',
+            backgroundColor: 'rgb(255, 99, 132)',
             data: arrayData,
           }],
           options: {}
         },
       });
+      var oggetto2 = {};
+      var venditeTotali = 0
+      for (var i = 0; i < data.length; i++) {
+        var salesman = data[i].salesman;
+        if (oggetto2[salesman] == undefined) {
+          oggetto2[salesman] = 0
+        }
+        oggetto2[salesman] += data[i].amount;
+        venditeTotali += data[i].amount
+      }
+      console.log(venditeTotali);
+      console.log(oggetto2);
+
+      var arrayLabelsPie = [];
+      var arrayDataPie = [];
+
+      for (var key2 in oggetto2) {
+        arrayLabelsPie.push(key2);
+        arrayDataPie.push((oggetto2[key2] / venditeTotali) * 100);
+      }
+
+      var ctx = $('.dashboard2')
+      var myLineChart = new Chart(ctx, {
+        type: 'pie',
+        data: {
+          datasets: [{
+            data: arrayDataPie,
+            backgroundColor: ['lightgreen', 'lightblue', 'lightyellow', 'lightcoral' ]
+          }],
+        labels: arrayLabelsPie
+        },
+      });
+
     },
     error: function(){
       alert('si è verificato un errore')
